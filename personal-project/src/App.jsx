d8f3012e0e08a6f303dda2fbe9a222608d1d2067
idamakela <idamakela98@gmail.com>
2023-02-21 10:55:16 +0100

    deleted unused pagacked and redid scss

--- personal-project/src/App.jsx
+++ personal-project/src/App.jsx
@@ -1,59 +1,26 @@
 import React, { useState } from 'react';
-import { FaDiceD20, FaPlusCircle } from 'react-icons/fa';
-import Selector from 'amount-selector.jsx';
+import { FaDiceD20 } from 'react-icons/fa';
 
 function dice(dice, amount) {
-
+    
 }
 
 
 function App() {
 
+    //If screen in mobile return below, else return "to big screen"
     return (
         <>
-            <header>
-                <div className="container">
-                    <FaDiceD20 className='dice-icon' />
-                    <h1>D20</h1>
-                </div>
-            </header>
             <main>
-                <div className='container one'>
-                    <div className="result">
-                        <h2 className='output'>20</h2>
-                        <h2>result</h2>
-                    </div>
+                <div className="result">
+                    <FaDiceD20 className='dice' />
                 </div>
-
-                <div className='container two'>
-                    <div className="select">
-                        <h2>Select</h2>
-
-                        <div className="container">
-                            <div className="dice">
-                                <div className="select-title">
-                                    <FaDiceD20 className='dice-icon' />
-                                    <h3>dice</h3>
-                                </div>
-
-                                <div className="select-dice">
-
-                                </div>
-                            </div>
-
-                            <div className="amount">
-                                <div className="amount-title">
-                                    <FaPlusCircle className='plus-icon' />
-                                    <h3>amount</h3>
-                                </div>
-
-                                <div className="select-amount">
-                                    <Selector />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="start"><h2>roll</h2></div>
+                <div className="container">
+                    <div className="text-container">
+                        <h1>D20 dice roller</h1>
+                        <p>some text about it</p>
                     </div>
+                    <button className='btn'>roll</button>
                 </div>
             </main>
         </>
--- personal-project/src/main.jsx
+++ personal-project/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-// import 'styles/fonts.css';
-import '../src/styles/index.scss'
+import './styles/fonts.css';
+import './styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
